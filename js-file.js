9f83ed250965a855e551d4ae7e76339058c6c7f1
JhollRibeiro <jrsantos.joao@gmail.com>
2023-09-14 17:43:37 -0300

    Update js-file.js

--- js-file.js
+++ js-file.js
@@ -1,6 +1,13 @@
-alert('Teste js');
-
 Notification.requestPermission().then(permission => {
-  //if(permission === 'granted')
   return;
 });
+
+
+if (Notification.permission === 'granted') {
+  new Notification("Gracias por enviar", {
+    body: "Ahora que ha actualizado sus datos, está listo para empezar.",
+    icon: "public-relation.png",
+    vibrate: [100, 50, 100],
+    tag: "onboarding",
+  })
+}
