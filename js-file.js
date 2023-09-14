51d5bd8310895985edb891a9271d4b42c01d717c
JhollRibeiro <jrsantos.joao@gmail.com>
2023-09-14 17:46:07 -0300

    Update js-file.js

--- js-file.js
+++ js-file.js
@@ -1,13 +1,18 @@
+
+let btnEmpezar;
+btnEmpezar = document.getElementById('continuarOnboarding');
+
 Notification.requestPermission().then(permission => {
   return;
 });
 
-
-if (Notification.permission === 'granted') {
-  new Notification("Gracias por enviar", {
-    body: "Ahora que ha actualizado sus datos, está listo para empezar.",
-    icon: "public-relation.png",
-    vibrate: [100, 50, 100],
-    tag: "onboarding",
-  })
+btnEmpezar.onclick = function (event) {
+  if (Notification.permission === 'granted') {
+    new Notification("Gracias por enviar", {
+      body: "Ahora que ha actualizado sus datos, está listo para empezar.",
+      icon: "public-relation.png",
+      vibrate: [100, 50, 100],
+      tag: "onboarding",
+    })
+  }
 }
