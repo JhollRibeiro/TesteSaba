072c02395f0b4904b9a031a495e2981e0333e404
JhollRibeiro <jrsantos.joao@gmail.com>
2023-09-14 17:47:05 -0300

    Update js-file.js

--- js-file.js
+++ js-file.js
@@ -1,4 +1,3 @@
-
 let btnEmpezar;
 btnEmpezar = document.getElementById('continuarOnboarding');
 
@@ -8,7 +7,7 @@ Notification.requestPermission().then(permission => {
 
 btnEmpezar.onclick = function (event) {
   if (Notification.permission === 'granted') {
-    new Notification("Gracias por enviar", {
+    new Notification("Gracias por enviar sus datos", {
       body: "Ahora que ha actualizado sus datos, está listo para empezar.",
       icon: "public-relation.png",
       vibrate: [100, 50, 100],
