faaf788f5f247d5eb0f4c00e394e13dd655556a9
JhollRibeiro <jrsantos.joao@gmail.com>
2023-09-19 16:58:19 -0300

    Update js-file.js

--- js-file.js
+++ js-file.js
@@ -5,14 +5,14 @@ Notification.requestPermission().then(permission => {
   return;
 });
 
-btnEmpezar.onclick = function (event) {
-  if (Notification.permission === 'granted') {
-    new Notification("Gracias por enviar sus datos",
-      {
-        body: "Ahora que ha actualizado sus datos, está listo para empezar.",
-        icon: "public-relation.png",
-        vibrate: [100, 50, 100],
-      }
-    );
-  }
-}
+// btnEmpezar.onclick = function (event) {
+//   if (Notification.permission === 'granted') {
+//     new Notification("Gracias por enviar sus datos",
+//       {
+//         body: "Ahora que ha actualizado sus datos, está listo para empezar.",
+//         icon: "public-relation.png",
+//         vibrate: [100, 50, 100],
+//       }
+//     );
+//   }
+// }
